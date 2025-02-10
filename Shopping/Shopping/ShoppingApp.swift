184fc129b59debef5a36a385f69e8286a7ab6f66
Eegii219 <Enkhtsetseg.Unurbayar@tachconsulting.tech>
2025-02-10 10:28:47 -0500

    initial commit, project Created.

---
+++ Shopping/Shopping/ShoppingApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShoppingApp.swift
+//  Shopping
+//
+//  Created by Enkhtsetseg Unurbayar on 2/10/25.
+//
+
+import SwiftUI
+
+@main
+struct ShoppingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
