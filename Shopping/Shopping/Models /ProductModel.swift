a67b9e11014acc8ab08971ea3d4fd1026cc10ef5
Tsetseg219 <enkhtsetseg.unurbayar@techconsulting.tech>
2025-02-11 09:20:24 -0500

    fixed 1

--- Shopping/Shopping/Models /ProductModel.swift
+++ Shopping/Shopping/Models /ProductModel.swift
@@ -7,7 +7,7 @@
 
 import Foundation
 
-struct ShoppingCartItem: Identifiable, Codable {
+struct ProductModel: Identifiable, Codable {
     let id: String
     let name: String
     let price: String
