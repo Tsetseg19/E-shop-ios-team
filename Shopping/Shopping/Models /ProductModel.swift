795a3053564aa941f34fb86796884da5c5fb1f84
Tsetseg219 <enkhtsetseg.unurbayar@techconsulting.tech>
2025-02-11 09:34:38 -0500

    Model

--- Shopping/Shopping/Models /ProductModel.swift
+++ Shopping/Shopping/Models /ProductModel.swift
@@ -5,3 +5,13 @@
 //  Created by Enkhtsetseg Unurbayar on 2/10/25.
 //
 
+import Foundation
+
+struct ProductModel: Identifiable, Codable {
+    let id: String
+    let name: String
+    let price: String
+    let imageURL: String
+    let stars: Int
+    let description: String
+}
